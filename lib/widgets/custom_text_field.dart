79be4a6ea35885a3b3a45fd3a2a67692cb2f1c8e
SamuelKamal <samuelkamal131@gmail.com>
2025-07-14 01:36:44 +0300

    create text field for add notes

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/constant.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hintText, this.maxLines = 1});
+
+  final String hintText;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        hintText: hintText,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: Colors.white, width: 2),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: kPrimaryColor, width: 2),
+        ),
+      ),
+    );
+  }
+}
