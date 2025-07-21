41a29a81449fe928c63673f37471e90c67ec1aaa
SamuelKamal <samuelkamal131@gmail.com>
2025-07-21 23:33:37 +0300

    create button for the modal sheet

--- lib/widgets/add_new_note.dart
+++ lib/widgets/add_new_note.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/constant.dart';
+import 'package:notesapp/widgets/custom_button.dart';
 import 'package:notesapp/widgets/custom_text_field.dart';
 
 class AddNewNote extends StatelessWidget {
@@ -9,14 +10,18 @@ class AddNewNote extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(hintText: 'Title'),
-          SizedBox(height: 16),
-          CustomTextField(hintText: 'Content', maxLines: 5),
-          SizedBox(height: 24),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextField(hintText: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(hintText: 'Content', maxLines: 5),
+            SizedBox(height: 60),
+            CustomButton(),
+            SizedBox(height: 32),
+          ],
+        ),
       ),
     );
   }
