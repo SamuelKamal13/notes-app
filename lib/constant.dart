69705e60045bd1dea5e48391feeba74ffcf28bd5
SamuelKamal <samuelkamal131@gmail.com>
2025-09-10 01:02:17 +0300

    create hive init and hive box

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Colors.lightBlueAccent;
+const kNoteModelBox = 'noteModelBox';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notesapp/constant.dart';
 import 'package:notesapp/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNoteModelBox);
+  
   runApp(const NotesApp());
 }
 
