174851e70bd1206b4f98c7278b011bbf8d69cd90
SamuelKamal <samuelkamal131@gmail.com>
2025-09-17 20:01:27 +0300

    take input and validate data

--- lib/widgets/add_new_note.dart
+++ lib/widgets/add_new_note.dart
@@ -9,18 +9,59 @@ class AddNewNote extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 32),
-            CustomTextField(hintText: 'Title'),
-            SizedBox(height: 16),
-            CustomTextField(hintText: 'Content', maxLines: 5),
-            SizedBox(height: 60),
-            CustomButton(),
-            SizedBox(height: 32),
-          ],
-        ),
+      child: SingleChildScrollView(child: FormWidget()),
+    );
+  }
+}
+
+class FormWidget extends StatefulWidget {
+  const FormWidget({super.key});
+
+  @override
+  State<FormWidget> createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<FormWidget> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(height: 32),
+          CustomTextField(
+            hintText: 'Title',
+            onSaved: (value) {
+              title = value;
+            },
+          ),
+          const SizedBox(height: 16),
+          CustomTextField(
+            hintText: 'Content',
+            maxLines: 5,
+            onSaved: (value) {
+              content = value;
+            },
+          ),
+          const SizedBox(height: 60),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(height: 32),
+        ],
       ),
     );
   }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,14 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:notesapp/constant.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hintText, this.maxLines = 1});
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    this.maxLines = 1,
+    this.onSaved,
+  });
 
   final String hintText;
   final int maxLines;
 
+  final void Function(String?)? onSaved;
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'Field is required';
+        } else {
+          return null;
+        }
+      },
       maxLines: maxLines,
       decoration: InputDecoration(
         hintText: hintText,
