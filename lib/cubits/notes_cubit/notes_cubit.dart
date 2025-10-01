4dc79f86e4d02d7ce7241bbeb4197fa6855f1327
SamuelKamal <samuelkamal131@gmail.com>
2025-10-01 15:29:26 +0300

    display notes part 1

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -9,14 +9,11 @@ part 'notes_state.dart';
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
+  List<NoteModel>? notes;
+
   fetchAllNotes() {
-    try {
-      var noteBox = Hive.box<NoteModel>(kNoteModelBox);
-      
-      List<NoteModel> notes = noteBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
-    }
+    var noteBox = Hive.box<NoteModel>(kNoteModelBox);
+
+    notes = noteBox.values.toList();
   }
 }
--- lib/widgets/notes_view_list.dart
+++ lib/widgets/notes_view_list.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notesapp/models/note_model.dart';
 import 'package:notesapp/widgets/note_item_wdget.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-        padding: const EdgeInsets.only(top: 0),
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return NotesCard();
-        },
-      ),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes ?? [];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+            padding: const EdgeInsets.only(top: 0),
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return NotesCard();
+            },
+          ),
+        );
+      },
     );
   }
 }
